23e0b0039ba9c99452750671ce0265fd1bccc289
Asli Yilmaz <asliyilmaz613@gmail.com>
2025-07-23 21:58:49 +0300

    Entitylerin oluşturulması

---
+++ ApiProject.WebApi/Entities/Service.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiProject.WebApi.Entities
+{
+    public class Service
+    {
+        public int ServiceId { get; set; }
+        public string ServiceTitle { get; set; }
+
+        public string ServiceDescription { get; set; }
+        public string IconUrl { get; set; }
+
+    }
+}
