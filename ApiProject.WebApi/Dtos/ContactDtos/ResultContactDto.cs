d6c967e82feaa7e610ac974906b6215937b08bdb
Asli Yilmaz <asliyilmaz613@gmail.com>
2025-08-01 14:01:45 +0300

    Dto ile manuel mapleme işlemi yapıldı-contact entitiy için

---
+++ ApiProject.WebApi/Dtos/ContactDtos/ResultContactDto.cs
@@ -0,0 +1,14 @@
+﻿namespace ApiProject.WebApi.Dtos.ContactDtos
+{
+    //dto lar result ön eki ile oluşturulur
+    //dto da id ile getirme, listeleme, güncelleme ve ekleme işlemleri yapılır silme işlemi yapılmaz
+    public class ResultContactDto
+    {
+        public int ContactId { get; set; }
+        public string MapLocation { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string OpenHours { get; set; }
+    }
+}
