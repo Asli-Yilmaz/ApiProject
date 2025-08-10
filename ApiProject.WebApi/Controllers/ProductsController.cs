db5fc7f117fbf989d8035bce905ec1391c69fa25
Asli Yilmaz <asliyilmaz613@gmail.com>
2025-08-10 13:51:56 +0300

    aralarında ilişki bulunan entityler için dto kullanılması

--- ApiProject.WebApi/Controllers/ProductsController.cs
+++ ApiProject.WebApi/Controllers/ProductsController.cs
@@ -1,5 +1,7 @@
 ﻿using ApiProject.WebApi.Context;
+using ApiProject.WebApi.Dtos.ProductDtos;
 using ApiProject.WebApi.Entities;
+using AutoMapper;
 using FluentValidation;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -12,11 +14,13 @@ namespace ApiProject.WebApi.Controllers
     {
         private readonly IValidator<Product> _validator;
         private readonly ApiContext _context;
+        private readonly IMapper _mapper;
 
-        public ProductsController(IValidator<Product> validator, ApiContext context)
+        public ProductsController(IValidator<Product> validator, ApiContext context, IMapper mapper)
         {
             _context = context;
             _validator = validator;
+            _mapper = mapper;
         }
         [HttpGet]
         public IActionResult ProductList() 
@@ -68,6 +72,14 @@ namespace ApiProject.WebApi.Controllers
                 _context.SaveChanges();
                 return Ok("Ürün güncelleme işlemi başarılı.");
             }
+        }
+        [HttpPost("CreateProductWithCategory")]
+        public IActionResult CreateProductWithCategory(CreateProductDto createProductDto)
+        {
+            var value=_mapper.Map<Product>(createProductDto);
+            _context.Products.Add(value);
+            _context.SaveChanges();
+            return Ok("Ürün ekleme işlemi başarılı.");
         }
     }
 }
