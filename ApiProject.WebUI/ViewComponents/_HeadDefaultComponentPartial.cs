f4d7c8808b5904b021fed930a95804e04c015c15
Asli Yilmaz <asliyilmaz613@gmail.com>
2025-08-13 13:06:22 +0300

    webui giriş

---
+++ ApiProject.WebUI/ViewComponents/_HeadDefaultComponentPartial.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProject.WebUI.ViewComponents
+{
+    public class _HeadDefaultComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        { return View(); }
+    }
+}
