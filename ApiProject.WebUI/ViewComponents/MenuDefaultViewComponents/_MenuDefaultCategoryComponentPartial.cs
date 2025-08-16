bd25009cf7a9a4db3a1e2cdb2bb74e02c6c5e74c
Asli Yilmaz <asliyilmaz613@gmail.com>
2025-08-16 16:02:57 +0300

    Menüdeki kategoriler dinamik olarak listelendi

---
+++ ApiProject.WebUI/ViewComponents/MenuDefaultViewComponents/_MenuDefaultCategoryComponentPartial.cs
@@ -0,0 +1,28 @@
+﻿using ApiProject.WebUI.Dtos.CategoryDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiProject.WebUI.ViewComponents.MenuDefaultViewComponents
+{
+    public class _MenuDefaultCategoryComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _MenuDefaultCategoryComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+            
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var clinet = _httpClientFactory.CreateClient();
+            var responseMessage = await clinet.GetAsync("https://localhost:7115/api/Categories");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData=await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
---
+++ ApiProject.WebUI/ViewComponents/MenuDefaultViewComponents/_MenuDefaultViewComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProject.WebUI.ViewComponents.MenuDefaultViewComponents
+{
+    public class _MenuDefaultViewComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
