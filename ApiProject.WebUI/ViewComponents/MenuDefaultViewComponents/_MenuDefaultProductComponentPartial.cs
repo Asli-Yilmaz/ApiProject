d5d1b763dec8c3454eb40e886b601ec4e6de5d8d
Asli Yilmaz <asliyilmaz613@gmail.com>
2025-08-17 11:46:10 +0300

    Menüdeki ürünler ui tarafında dinamik olarak listelendi

---
+++ ApiProject.WebUI/ViewComponents/MenuDefaultViewComponents/_MenuDefaultProductComponentPartial.cs
@@ -0,0 +1,28 @@
+﻿using ApiProject.WebUI.Dtos.ProductDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiProject.WebUI.ViewComponents.MenuDefaultViewComponents
+{
+    public class _MenuDefaultProductComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _MenuDefaultProductComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+            
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7115/api/Products");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var values= await responseMessage.Content.ReadAsStringAsync();
+                var jsonData = JsonConvert.DeserializeObject<List<ResultProductDtos>>(values);
+                return View(jsonData);
+            }
+            return View();
+        }
+    }
+}
