1ebd6c7d65adfc028cad4c4c5f14d1048362a22a
Asli Yilmaz <asliyilmaz613@gmail.com>
2025-08-17 12:50:30 +0300

    Testimonials kısmına ait controller, partial view ve dto yapıları eklendi

---
+++ ApiProject.WebApi/Controllers/TestimonialsController.cs
@@ -0,0 +1,59 @@
+﻿using ApiProject.WebApi.Context;
+using ApiProject.WebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestimonialsController : ControllerBase
+    {
+        private readonly ApiContext _context;
+        public TestimonialsController(ApiContext context)
+        {
+            _context = context;
+        }
+        //dependency injection için constract methodlarının  registerationu yapılmalı 
+        //bu da program.cs dosyasına : builder.Services.AddDbContext<ApiContext>(); satırı eklenir.
+
+        [HttpPost]
+        public IActionResult CreateCategory(Testimonial testimonial)
+        {
+            _context.Testimonials.Add(testimonial);
+            _context.SaveChanges();
+            return Ok("Referans Ekleme İşlemi Başarılı.");
+        }
+        [HttpGet]
+        public IActionResult ListTestimonial()
+        {
+            var values = _context.Testimonials.ToList();
+            return Ok(values);
+        }
+        [HttpDelete]
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = _context.Testimonials.Find(id);
+            _context.Testimonials.Remove(value);
+            _context.SaveChanges();
+            return Ok("Referans silme işlemi başarılı.");
+        }
+        //bir controller içinde aynı attribute türünü birden fazla kullanamazsın hata verir bu yüzden bir ad(root) verilir
+        //[HttpGet] ->böyle olsaydı hata alınırdı
+        [HttpGet("GetTestimonial")]
+        public IActionResult GetTestimonial(int id)
+        {
+            var value = _context.Testimonials.Find(id);
+            return Ok(value);
+        }
+        [HttpPut]
+        public IActionResult UpdateTestimonial(Testimonial testimonial)
+        {
+
+            _context.Testimonials.Update(testimonial);
+            _context.SaveChanges();
+            return Ok("Referans güncelleme işlemi başarılı.");
+
+        }
+    }
+}
---
+++ ApiProject.WebUI/ViewComponents/_TestimonialDefaultComponentPartial.cs
@@ -0,0 +1,28 @@
+﻿using ApiProject.WebUI.Dtos.TestimonialDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiProject.WebUI.ViewComponents
+{
+    public class _TestimonialDefaultComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _TestimonialDefaultComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7115/api/Testimonials");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var values=await responseMessage.Content.ReadAsStringAsync();
+                var jsonData = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(values);
+                return View(jsonData);
+            }
+            return View();
+        }
+    }
+}
