597fd76301661ee95204782e1b9e91a62fa63a59
Asli Yilmaz <asliyilmaz613@gmail.com>
2025-08-18 12:30:25 +0300

    chef entity için dto,view partial oluşturuldu dinamik yapı sağlandı

---
+++ ApiProject.WebUI/ViewComponents/_ChefDefaultComponentPartial.cs
@@ -0,0 +1,27 @@
+﻿using ApiProject.WebUI.Dtos.ChefDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiProject.WebUI.ViewComponents
+{
+    public class _ChefDefaultComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _ChefDefaultComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7115/api/Chefs");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var values = await responseMessage.Content.ReadAsStringAsync();
+                var jsonData = JsonConvert.DeserializeObject<List<ResultChefDto>>(values);
+                return View(jsonData);
+            }
+            return View();
+        }
+    }
+}
