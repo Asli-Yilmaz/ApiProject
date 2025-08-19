2f8f592ad1f423b67604980936305f78628eb8e9
Asli Yilmaz <asliyilmaz613@gmail.com>
2025-08-19 13:57:19 +0300

    navbar partial view ve navbar partial view içinde formInline partial view oluşturuldu

---
+++ ApiProject.WebUI/ViewComponents/AdminLayoutNavbarComponents/_NavbarFormInlineAdminLayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProject.WebUI.ViewComponents.AdminLayoutNavbarComponents
+{
+    public class _NavbarFormInlineAdminLayoutComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
