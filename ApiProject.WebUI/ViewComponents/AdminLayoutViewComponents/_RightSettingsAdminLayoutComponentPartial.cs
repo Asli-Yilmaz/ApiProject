34eec2ba2b4c8f84e244cb3ed347074ee6996396
Asli Yilmaz <asliyilmaz613@gmail.com>
2025-09-02 21:33:05 +0300

    right-left sidebar partial view ve render body oluşturuldu

---
+++ ApiProject.WebUI/ViewComponents/AdminLayoutViewComponents/_RightSettingsAdminLayoutComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace ApiProject.WebUI.ViewComponents.AdminLayoutViewComponents
+{
+    public class _RightSettingsAdminLayoutComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
