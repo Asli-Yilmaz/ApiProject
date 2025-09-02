dc813166c17b64a0a90e0807f85f6fa9bf8ab71b
Asli Yilmaz <asliyilmaz613@gmail.com>
2025-09-02 23:34:42 +0300

    CategoryList view kısmı düzenlendi

--- ApiProject.WebUI/Controllers/CategoryController.cs
+++ ApiProject.WebUI/Controllers/CategoryController.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApiProject.WebUI.Dtos.CategoryDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace ApiProject.WebUI.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult CategoryList()
+        private IHttpClientFactory _httpClientFactory;
+
+        public CategoryController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> CategoryList()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7115/api/Categories");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var value= await responseMessage.Content.ReadAsStringAsync();
+                var jsonData = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(value);
+                return View(jsonData);
+            }
             return View();
         }
     }
