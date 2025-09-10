0501da31939483f5f4d4e7198ec3f624e810cf81
Asli Yilmaz <asliyilmaz613@gmail.com>
2025-09-10 11:47:44 +0300

    About sayfasına ait ui işlemleri tamamlandı

---
+++ ApiProject.WebUI/Dtos/AboutDtos/GetAboutByIdDto.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiProject.WebUI.Dtos.AboutDtos
+{
+    public class GetAboutByIdDto
+    {
+        public int Aboutid { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageURL { get; set; }
+        public string VideoCoverImageURL { get; set; }
+        public string VideoURL { get; set; }
+        public string ReservetionNumber { get; set; }
+    }
+}
