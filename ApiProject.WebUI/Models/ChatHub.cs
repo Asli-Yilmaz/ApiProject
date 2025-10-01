900e2e27a034d60de78b3ca8d14e729be90423ee
Asli Yilmaz <asliyilmaz613@gmail.com>
2025-10-01 15:50:09 +0300

    chathub smodeli oluşturuldu, onconnect ve ondisconnet metotları yazıldı

---
+++ ApiProject.WebUI/Models/ChatHub.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ApiProject.WebUI.Models
+{
+    public class ChatHub:Hub
+    {
+        private const string apiKey = "";
+        private const string model = "gpt-4o-mini";
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ChatHub(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        private static readonly Dictionary<string, List<Dictionary<string, string>>> _history = new();
+
+        public override Task OnConnectedAsync()
+        {
+            _history[Context.ConnectionId] = new List<Dictionary<string, string>>
+            {
+                new Dictionary<string, string>
+                {
+                    ["role"]="system",
+                    ["content"]="You are a Helpful assistant. Keep answers concise"
+                }
+            };
+            return base.OnConnectedAsync();
+        }
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            _history.Remove(Context.ConnectionId);
+            return base.OnDisconnectedAsync(exception);
+        }
+        //public async Task SendMessage(string userMessage)
+        //{
+
+        //}
+
+    }
+}
