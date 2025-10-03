a73c3f03586a9ede3ce9bbf4d196af10f00270a5
Asli Yilmaz <asliyilmaz613@gmail.com>
2025-10-03 10:40:09 +0300

    dashboard controller ve dashboard widgets partial view oluşturuldu

---
+++ ApiProject.WebUI/ViewComponents/DashboardWidgetsComponents/_DashboardWidgetsComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProject.WebUI.ViewComponents.DashboardWidgetsComponents
+{
+    public class _DashboardWidgetsComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
