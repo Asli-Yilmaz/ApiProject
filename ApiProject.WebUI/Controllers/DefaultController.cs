98c9e235beb83abcffbe7bc806c3a11c0b0ba01a
Asli Yilmaz <asliyilmaz613@gmail.com>
2025-11-15 11:37:12 +0300

    reservasyon sayfası düzenlendi

--- ApiProject.WebUI/Controllers/DefaultController.cs
+++ ApiProject.WebUI/Controllers/DefaultController.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApiProject.WebUI.Dtos.ReservationDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace ApiProject.WebUI.Controllers
 {
     public class DefaultController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DefaultController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateReservationDto createReservationDto)
+        {
+            createReservationDto.ReservationStatus = "Onay Bekliyor";
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createReservationDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7115/api/Reservations", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+
         }
     }
 }
